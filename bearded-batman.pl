48cc07aed2a7e8e5ba59740ce57596f6fea62ecc
paul <paul@rundkorn.org>
2014-08-15 12:36:36 +0200

    Settled basic program flow

---
+++ bearded-batman.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use warnings;
+
+
+require "services.pl";
+
+
+if($#ARGV != 0) {
+	print "Usage: ./bearded-batman.pl [username]\n";
+	exit;
+}
+
+my $name = $ARGV[0];
+
+while(my ($key, $value) = each %services) {
+	print "$key: ";
+
+	if($value->($name)) {
+		print "taken\n";
+	}
+	else {
+		print "available\n";
+	}
+}
