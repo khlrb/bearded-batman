6265fad8623985b1912f2e6385df4174cd1fe762
paul <paul@rundkorn.org>
2014-08-15 12:37:38 +0200

    Added some services

---
+++ services.pl
@@ -0,0 +1,17 @@
+use LWP::Simple;
+
+our %services = (
+	"about.me" => sub { return head("http://about.me/@_"); },
+	"advogato.org" => sub { return head("http://www.advogato.org/person/@_"); },
+	"deviantART" => sub { return head("http://@_.deviantart.com"); },
+	"flickr" => sub { return head("http://flickr.com/photos/@_"); },
+	"foursquare" => sub { return head("http://foursquare.com/@_"); },
+	"instagram" => sub { return head("http://instagram.com/@_"); },
+	"last.fm" => sub { return head("http://lastfm.com/user/@_"); },
+	"livejournal" => sub { return head("http://@_.livejournal.com"); },
+	"twitter" => sub { return head("http://twitter.com/@_"); },
+	"tumblr" => sub { return head("http://@_.tumblr.com"); },
+	"soundcloud" => sub { return head("http://soundcloud.com/@_"); },
+	"pinterest" => sub { return head("http://www.pinterest.com/@_"); }
+);
+
